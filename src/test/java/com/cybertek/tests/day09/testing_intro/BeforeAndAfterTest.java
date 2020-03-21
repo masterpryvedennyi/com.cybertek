ac84d5824b8ae4a6c088ab79675b7d2dd0763426
pryvedennyi <pryvedennyi@gmail.com>
2020-03-21 12:55:42 -0400

    update

--- src/test/java/com/cybertek/tests/day09/testing_intro/BeforeAndAfterTest.java
+++ src/test/java/com/cybertek/tests/day09/testing_intro/BeforeAndAfterTest.java
@@ -1,18 +1,34 @@
 package com.cybertek.tests.day09.testing_intro;
 
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Test;
+import org.testng.Assert;
+import org.testng.annotations.*;
 
 public class BeforeAndAfterTest {
+
+    @BeforeClass
+    public void beforeClass(){
+        System.out.println("Before class");
+    }
+
+    @AfterClass
+    public void afterClass(){
+        System.out.println("After class");
+    }
 
     @BeforeMethod
     public void beforeMethod(){
         System.out.println("Before method");
     }
 
+    @AfterMethod
+    public void afterMethod(){
+        System.out.println("After method");
+    }
+
     @Test
     public void test1(){
         System.out.println("this is test1");
+        Assert.assertEquals(true, false);
     }
 
     @Test
--- src/test/java/com/cybertek/utilities/WebDriverFactory.java
+++ src/test/java/com/cybertek/utilities/WebDriverFactory.java
@@ -4,6 +4,7 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.safari.SafariDriver;
 
 public class WebDriverFactory {
     // write a return method which returns webdriver
@@ -12,9 +13,11 @@ public class WebDriverFactory {
         if (browser.equalsIgnoreCase("chrome")) {
             WebDriverManager.chromedriver().setup();
             driver = new ChromeDriver();
-        } else if (browser.equalsIgnoreCase("firefox")) {
+        }else if (browser.equalsIgnoreCase("firefox")) {
             WebDriverManager.firefoxdriver().setup();
             driver = new FirefoxDriver();
+        }else if (browser.equalsIgnoreCase("safari")) {
+            driver = new SafariDriver();
         }
         return driver;
     }
